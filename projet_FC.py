af1d908cbd52e2c4c6a08ac3e4c21c05bbd6ba1f
Johnny Joseph <johnnyjosephpro@gmail.com>
2019-09-11 15:57:11 -0400

    Add new lign

--- projet_FC.py
+++ projet_FC.py
@@ -1,6 +1,36 @@
 from tkinter import *
+from pip import *
 
 window1 = Tk()
+
+# Function Child Window
+def open_window2():
+    window2 = Tk()
+    # child = Toplevel()
+    window2.title('GESTION DES EMPLOYES')
+    window2.geometry("700x400")
+    window2.resizable(width=False, height=False)
+    window2.config(bg='#4065A4')
+
+    # Title
+    titre = Label(window2, text="GESTION DES EMPLOYES", font=("Century Gothy", 18), bg="#4065A4", fg="WHITE")
+    titre.place(x=180, y=10)
+    titre1= Label(window2, text="ID Employe:",font=("Open Sans", 13), bg="#4065A4", fg="WHITE").place(x=20, y=50)
+    titre2= Label(window2, text="Nom:",font=("Open Sans", 13), bg="#4065A4", fg="WHITE").place(x=20, y=80)
+    titre3= Label(window2, text="Prenom:",font=("Open Sans", 13), bg="#4065A4", fg="WHITE").place(x=20, y=110)
+    titre4= Label(window2, text="Date de naissance:",font=("Open Sans", 13), bg="#4065A4", fg="WHITE").place(x=20, y=140)
+    titre5= Label(window2, text="Lieu de naissance:",font=("Open Sans", 13), bg="#4065A4", fg="WHITE").place(x=20, y=170)
+    titre6= Label(window2, text="Adresse:",font=("Open Sans", 13), bg="#4065A4", fg="WHITE").place(x=20, y=200)
+    # titre7= Label(frame1, text="Email:",font=("Open Sans", 13), bg="#4065A4", fg="WHITE").pack()
+    # titre8= Label(frame1, text="Telephone:",font=("Open Sans", 13), bg="#4065A4", fg="WHITE").pack()
+
+    # Buttons
+    # button_quit = Button(child, text="QUITTER", height="1", width="7", font=("Open Sans", 15), bg="WHITE", fg="#44749D", command=child.destroy).grid()
+    # button_save = Button(child, text="SAVE", height="1", width="7", font=("Open Sans", 15), bg="WHITE", fg="#44749D").grid()
+    
+    
+    
+
 
 # Personalisation Fenetre
 window1.title('Syteme de Gestion des employes')
@@ -11,11 +41,12 @@ cadre = Frame(window1, bg="#4065A4").pack(side=TOP)
 
 # Create titles project
 title = Label(cadre, text="\nPROJET DE FIN DE SESSION", font=("Century Gothy", 30), bg="#4065A4", fg="WHITE").pack()
-title = Label(cadre, text="\nMATIERE: Python", font=("Open Sans", 15), bg="#4065A4", fg="WHITE").pack()
-title = Label(cadre, text="Systeme de Gestion des employes", font=("Open Sans", 12), bg="#4065A4",fg="WHITE").pack()
+title = Label(cadre, text="\nPython", font=("Lato Bold", 20), bg="#4065A4", fg="WHITE").pack()
+title = Label(cadre, text="Systeme de Gestion de RH", font=("Open Sans", 14), bg="#4065A4",fg="WHITE").pack()
 title = Label(cadre, text="Etudiant: Johnny JOSEPH\n\n", font=("Open Sans", 12), bg="#4065A4", fg="WHITE").pack()
 
 # Create Button
-button_suivant = Button(cadre, text="SUIVANT", height="1", width="15", font=("Open Sans", 20), bg="WHITE", fg="#44749D").pack()
+button_suivant = Button(cadre, text="SUIVANT", height="1", width="10", font=("Open Sans", 20), bg="WHITE", fg="#44749D", command=open_window2).pack(side=RIGHT, padx=15, pady=15)
 
 window1.mainloop()
+window2.mainloop()
