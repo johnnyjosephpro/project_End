e9acf527626aa585806084e93e42687f3871d3c2
Johnny Joseph <johnnyjosephpro@gmail.com>
2019-09-18 21:36:02 -0400

    Modify position and width of button image

--- projet_FC.py
+++ projet_FC.py
@@ -2,12 +2,10 @@ from tkinter import *
 import mysql.connector
 import tkinter as tk
 from tkinter import filedialog
-
 # from PIL import Image, ImageTk
 
-
 # Function create Database and create a Table
-def create_database():
+def connectiondb():
     mdb = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -15,11 +13,10 @@ def create_database():
     )
     requete1 = "CREATE DATABASE project_db"
     requete2 = "CREATE TABLE employe (ID int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,nom varchar(20) NOT NULL,prenom varchar(20) NOT NULL,date_naissance date,lieu_naissance varchar(20),adresse varchar(50),email varchar(20),telephone varchar(15))"
-    mycursor = mdb.cursor(requete1, requete2)
-    mycursor.execute("")
-    print("Data Base create")
+    mycursor = mdb.cursor()
+    mycursor.execute("Create DataBase project_db")
     mdb.commit()
-
+    mdb.close()
 
 # Function Enregistrement
 # def enregistrer():
@@ -36,10 +33,14 @@ def open_window2():
     window2.resizable(width=False, height=False)
     window2.config(bg='#3b3b3b')
     window2.wm_iconbitmap('icon.ico')
+    
+    # Variable
+    sexe = StringVar()
 
     # Frame for Image
     photo_frame = LabelFrame(window2, width=200, height=200, bd=2, relief='ridge').place(x=500, y=140)
     # img = PhotoImage(file="\steps-icon-1.png")
+
 
     # Title of child window
     titre = Label(window2, text="GESTION DES EMPLOYES", font=("Century Gothic Bold", 20), bg="#3b3b3b",fg="#FFCD00").place(x=200, y=10)
@@ -60,10 +61,12 @@ def open_window2():
     entrer2.place(x=35, y=140)
     entrer3 = Entry(window2, width="30")
     entrer3.place(x=295, y=140)
-    sexe = tk.IntVar()
-    buttonM = Radiobutton(window2, text="Masculin", variable=sexe, value=0, font=("Century Gothic", 12), fg="WHITE", bg="#3b3b3b", command=sexe.get()).place(x=35, y=195)
-    buttonF = Radiobutton(window2, text="Feminin", variable=sexe, value=1, font=(
-        "Century Gothic", 12), fg="WHITE", bg="#3b3b3b", command=sexe.get()).place(x=140, y=195)
+    sexechoice = sexe.get()
+    
+    buttonM = Radiobutton(window2, text="Masculin", value="masculin", variable= sexe, font=(
+        "Century Gothic", 12), fg="WHITE", bg="#3b3b3b").place(x=35, y=195)
+    buttonF = Radiobutton(window2, text="Feminin", value="feminin", variable= sexe, font=(
+        "Century Gothic", 12), fg="WHITE", bg="#3b3b3b").place(x=140, y=195)
     entrer4 = Entry(window2, width="30").place(x=295, y=200)
     entrer5 = Entry(window2, width="30")
     entrer5.place(x=35, y=260)
@@ -91,7 +94,8 @@ def open_window2():
             initialdir="/", title="Select Image", filetype=(("jpeg", "*.jpg"), ("All Files", "*.*")))
 
     # Button of child window
-    button_image = Button(window2, text="Parcourir", command=file_Dialog).place(x=500, y=100)
+    button_image = Button(window2, text="Ajouter Photo", height="1", width="24",font=(
+        "Century Gothic Bold", 10),fg="#44749D", command=file_Dialog).place(x=500, y=100)
     button_Enregistrer = Button(window2, text="ENREGISTRER", height="1", width="12", font=("Century Gothic Bold", 13),
                                 bg="WHITE", fg="#44749D").place(x=240, y=380)
     button_Rechercher = Button(window2, text="RECHERCHER", height="1", width="12", font=("Century Gothic Bold", 13),
@@ -113,6 +117,7 @@ window1.resizable(width=False, height=False)
 window1.config(background='#3b3b3b')
 window1.wm_iconbitmap('icon.ico')
 
+
 # Create titles for Main Window
 title = Label(window1, text="PROJET DE FIN DE SESSION", font=("Century Gothic bold", 22), bg="#3b3b3b",
               fg="#FFCD00").place(x=170, y=10)
@@ -130,6 +135,7 @@ title4 = Label(window1, text="Rodely DOUILLY", font=("Century Gothic Bold", 14),
 title5 = Label(window1, text="Option:", font=("Century Gothic", 15), bg="#3b3b3b", fg="WHITE").place(x=30, y=180)
 title5 = Label(window1, text="Full Stack Web Developer", font=("Century Gothic Bold", 14), bg="#3b3b3b",
                fg="WHITE").place(x=140, y=180)
+
 
 # Create Button for Main Window
 button_suivant = Button(window1, text="SUIVANT", height="1", width="10", font=("Century Gothic Bold", 14), bg="WHITE",
